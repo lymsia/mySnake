14ed5e411c066414efb168762e0747022da66a53
Roy <lymsia.wijaya@gmail.com>
2019-06-24 13:38:26 +0900

    Initial Commit

---
+++ SnameTests/SnameTests.swift
@@ -0,0 +1,34 @@
+//
+//  SnameTests.swift
+//  SnameTests
+//
+//  Created by Roy Wijaya on 2019/06/24.
+//  Copyright © 2019 Roy Wijaya. All rights reserved.
+//
+
+import XCTest
+@testable import Sname
+
+class SnameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
